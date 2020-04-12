ed197c1db683e815391afb8a57381a9166964772
Khamthalar5566 <tikkhamthalar@gmail.com>
2020-04-12 13:19:45 +0700

    complate des lpb

--- src/app/components/luangprabang-des/luangprabang-des.component.ts
+++ src/app/components/luangprabang-des/luangprabang-des.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-luangprabang-des',
@@ -9,7 +9,55 @@ export class LuangprabangDesComponent implements OnInit {
 
   constructor() { }
 
+  CAROUSEL_BREAKPOINT = 768;
+  carouselDisplayMode = 'multiple';
+
+  cards = [
+    {
+      img: '/assets/des/lpb/001.png'
+    },
+    {
+      img: '/assets/des/lpb/002.png'
+    },
+    {
+      img: '/assets/des/lpb/003.png'
+    },
+    {
+      img: '/assets/des/lpb/004.png'
+    },
+    {
+      img: '/assets/des/lpb/005.png'
+    },
+    {
+      img: '/assets/des/lpb/006.png'
+    },
+  ];
+  slides: any = [[]];
+  chunk(arr, chunkSize) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
+
   ngOnInit(): void {
+    this.slides = this.chunk(this.cards, 3);
+
+    if (window.innerWidth <= this.CAROUSEL_BREAKPOINT) {
+      this.carouselDisplayMode = 'single';
+    } else {
+      this.carouselDisplayMode = 'multiple';
+    }
+  }
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    if (window.innerWidth <= this.CAROUSEL_BREAKPOINT) {
+      this.carouselDisplayMode = 'single';
+    } else {
+      this.carouselDisplayMode = 'multiple';
+    }
   }
 
 }
